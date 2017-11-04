8ad5cd4f72b055b2845109d060fbf66712d0af86
spckayak <19294868+spckayak@users.noreply.github.com>
2017-11-04 11:59:56 -0700

    Added Python Files

---
+++ classconnect.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/trigger', methods=['POST'])
+def trigger():
+	if (request.data): #If a json object was recieved
+		req_data = request.get_json()
+		try:
+			apitoken = req_data['apitoken']
+			repo = req_data['repo']
+			branch = req_data['branch']
+		except:
+			return("Post request recieved, but not all parameters were provided")
+
+		if (apitoken) and (repo)and (branch):
+			pullCmd="git clone https://08da1b41be17100cdad1948276feb081a2d1aabd@github.com/spckayak/ClassConnectWebServer.git"
+			os.system("cd /home/ubuntu")
+			os.system(pullCmd)
+			return("Post Request Succesfull")
+	else:
+		return("Json Object was not recieved")
+
+@app.route('/gitwebhook', methods=['POST'])
+def webhook():
+	req_data = request.get_json()
+	pullCmd="git clone https://08da1b41be17100cdad1948276feb081a2d1aabd@github.com/spckayak/ClassConnectWebServer.git"
+	os.system("cd /home/ubuntu")
+	os.system(pullCmd)
+	return("OK")
