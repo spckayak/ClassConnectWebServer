af0017a097b52bb4d255afb06aeed759c8eaeae6
ericle77426 <32599688+ericle77426@users.noreply.github.com>
2017-12-08 21:32:25 -0800

    Update classconnect.py

--- classconnect.py
+++ classconnect.py
@@ -16,6 +16,10 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route("/logins.html")
+def login():
+    return render_template('logins.html')
+
 @app.route("/dashboard.html")
 def dashboard():
     return render_template('dashboard.html')
