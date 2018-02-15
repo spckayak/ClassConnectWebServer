27429a49d3e8c87922cfd492960ab4aa430d6333
spckayak <19294868+spckayak@users.noreply.github.com>
2018-02-14 23:14:40 -0800

    t

--- mysql.py
+++ mysql.py
@@ -1,13 +1,11 @@
 import os, MySQLdb, vars
 
-def loginSubmit():
-
-	config = "user='%s',passwd='%s',host='%s', port='3306', db='StudentLogin'" % (vars.user, vars.password, vars.host)
-	#username = request.form.get('username', None)
-	#username = request.form.get('password', None)
-	db = MySQLdb.connect(config)
-	cursor = db.cursor()
-	cursor.execute("SELECT VERSION()")
-	data = cursor.fetchall()
-	db.close()
-	return (data)
+config = "user='%s',passwd='%s',host='%s', port='3306', db='StudentLogin'" % (vars.user, vars.password, vars.host)
+#username = request.form.get('username', None)
+#username = request.form.get('password', None)
+db = MySQLdb.connect(config)
+cursor = db.cursor()
+cursor.execute("SELECT VERSION()")
+data = cursor.fetchall()
+db.close()
+print (data)
