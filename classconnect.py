e988544a13c63aeefcc13758a7aa9259238559f2
spckayak <19294868+spckayak@users.noreply.github.com>
2018-02-15 01:39:06 -0800

    1

--- classconnect.py
+++ classconnect.py
@@ -64,7 +64,7 @@ def loginSubmit():
 	cur.execute(command)
 	result= cur.fetchone()
 	db.close()
-	if cur.rowcount and passwordReq == result[0]: #If an entry was matched
+	if cur.rowcount and passwordReq == result[0]: #If an entry was recieved, and the password matched
 		return redirect(url_for('index.html'))
 	return render_template('login.html')
 
