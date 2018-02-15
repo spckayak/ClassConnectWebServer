625536c834c9101e60ea0a57990b9d50203d2a45
spckayak <19294868+spckayak@users.noreply.github.com>
2018-02-14 23:27:25 -0800

    1

--- mysql.py
+++ mysql.py
@@ -3,9 +3,9 @@ import os, MySQLdb, vars
 #config = "user='%s',passwd='%s',host='%s', port='3306', db='StudentLogin'" % (vars.user, vars.password, vars.host)
 #username = request.form.get('username', None)
 #username = request.form.get('password', None)
-db = MySQLdb.connect(host = "%s",
-					user="%s",
-					passwd="%s",
+db = MySQLdb.connect(host = %s,
+					user= %s,
+					passwd= %s,
 					db="StudentLogin" % (vars.host, vars.user, vars.password))
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
