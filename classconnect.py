7e434545e39e617f9170194d2f0e7d3aee8ca852
spckayak <19294868+spckayak@users.noreply.github.com>
2018-04-25 19:18:09 -0700

    Update classconnect.py

--- classconnect.py
+++ classconnect.py
@@ -152,25 +152,6 @@ def accountCreate():
 	else:	
 		message = "Unexpected error has occured in checking existing email."
 		return render_template('register.html', message=message)
-	
-@app.route('/trigger', methods=['POST']) #For Jenkins webhook
-def trigger():
-	if (request.data): #If a json object was recieved
-		req_data = request.get_json()
-		try:
-			apitoken = req_data['apitoken']
-			repo = req_data['repo']
-			branch = req_data['branch']
-		except:
-			return("Post request recieved, but not all parameters were provided")
-
-		if (apitoken) and (repo)and (branch):
-			pullCmd="git clone https://08da1b41be17100cdad1948276feb081a2d1aabd@github.com/spckayak/ClassConnectWebServer.git"
-			os.system("cd /home/ubuntu")
-			os.system(pullCmd)
-			return("Post Request Succesfull")
-	else:
-		return("Json Object was not recieved")
 
 @app.route('/gitwebhook', methods=['POST'])
 def webhook():
