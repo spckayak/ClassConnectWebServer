1931af73e14e3ab5a57cd512ebbf865b3bb81213
spckayak <19294868+spckayak@users.noreply.github.com>
2018-05-04 21:10:48 -0700

    Update classconnect.py

--- classconnect.py
+++ classconnect.py
@@ -83,9 +83,9 @@ def dashboard():
 		classSect = result[1]
 		classSeme = result[2]
 		classYear = result[3]
-		#insertBox = "'%s','%s','%s','%s'" % (className,classSect,classSeme,classYear)
+		insertBox = "'%s','%s','%s','%s'" % (className,classSect,classSeme,classYear)
 		classlist = " "
-		#insertBox = "<div class=\"info-box-content\"><span class=\"info-box-text\"><a href=\"task.html\">'%s'</a></span><span class=\"info-box-number\">'%s' - '%s' '%s' 2017</span><span class=\"info-box-number\">Attendance today</span></div>" % (className,classSect,classSeme,classYear)
+		insertBox = "<div class=\"info-box-content\"><span class=\"info-box-text\"><a href=\"task.html\">'%s'</a></span><span class=\"info-box-number\">'%s' - '%s' '%s' 2017</span><span class=\"info-box-number\">Attendance today</span></div>" % (className,classSect,classSeme,classYear)
 		classlist = classlist #+ insertBox    
     classlist=Markup(classlist)
     return render_template('dashboard.html', fname=fname, classlist=classlist)
